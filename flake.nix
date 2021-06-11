816b8196027152bd91eca1160b69f338a9deb143
Bruno BELANYI <bruno@belanyi.fr>
2021-06-12 00:38:17 +0200

    nix: add flake

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "Ambroisie's blog";
+
+  inputs = {
+    futils = {
+      type = "github";
+      owner = "numtide";
+      repo = "flake-utils";
+      ref = "master";
+    };
+
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixpkgs-unstable";
+    };
+
+    pre-commit-hooks = {
+      type = "github";
+      owner = "cachix";
+      repo = "pre-commit-hooks.nix";
+      ref = "master";
+      inputs = {
+        flake-utils.follows = "futils";
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = { self, futils, nixpkgs, pre-commit-hooks } @ inputs:
+    futils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs) lib;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        checks = {
+          pre-commit = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+
+            hooks = {
+              nixpkgs-fmt = {
+                enable = true;
+              };
+
+              shellcheck = {
+                enable = true;
+              };
+            };
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          name = "matrix-notifier";
+
+          buildInputs = with pkgs; [
+            curl
+            jq
+          ];
+
+          inherit (self.checks.${system}.pre-commit) shellHook;
+        };
+      }
+    );
+}
