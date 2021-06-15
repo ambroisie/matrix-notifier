75d482cd6b0045fa463f0375d2bdf4b2ea4a3d1d
Bruno BELANYI <bruno@belanyi.fr>
2021-06-15 18:39:19 +0200

    flake: bump version to v0.1.2

--- flake.nix
+++ flake.nix
@@ -75,7 +75,7 @@
         packages = {
           matrix-notifier = pkgs.stdenvNoCC.mkDerivation rec {
             pname = "matrix-notifier";
-            version = "0.1.1";
+            version = "0.1.2";
 
             src = ./matrix-notifier;
 
